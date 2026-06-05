0f43c677dd045ac3cfc00e1f7bcedabb3f6363a0
tianxiao <tianxiao@baai.ac.cn>
2026-06-05 09:47:12 +0800

    Update operation name in scatter_reduce tests
    Signed-off-by: tianxiao <tianxiao@baai.ac.cn>

--- benchmark/test_scatter_reduce_two.py
+++ benchmark/test_scatter_reduce_two.py
@@ -19,7 +19,7 @@ def _input_fn_factory(reduce):
 @pytest.mark.scatter_reduce_two_
 def test_scatter_reduce_two_inplace_sum():
     bench = base.GenericBenchmark2DOnly(
-        op_name="scatter_reduce_.sum",
+        op_name="scatter_reduce_",
         torch_op=torch.Tensor.scatter_reduce_,
         input_fn=_input_fn_factory("sum"),
         dtypes=consts.FLOAT_DTYPES,
@@ -31,7 +31,7 @@ def test_scatter_reduce_two_inplace_sum():
 @pytest.mark.scatter_reduce_two_
 def test_scatter_reduce_two_inplace_amax():
     bench = base.GenericBenchmark2DOnly(
-        op_name="scatter_reduce_.amax",
+        op_name="scatter_reduce_",
         torch_op=torch.Tensor.scatter_reduce_,
         input_fn=_input_fn_factory("amax"),
         dtypes=consts.FLOAT_DTYPES,
